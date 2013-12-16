0d95a99efccef44487fe4293b00f749fbb345051
Diego Zanella <businessdad75@gmail.com>
2013-12-16 17:26:40 +0000

    Added task execution tracker class

--- CronJobs/lib/classes/scheduling/class.crontasktracker.php
+++ CronJobs/lib/classes/scheduling/class.crontasktracker.php
@@ -2,20 +2,45 @@
 if (!defined('APPLICATION')) exit();
 
 class TaskTracker {
+	const EPOCH = 0;
+
+	// @var string The task ID.
 	protected $CronTaskID;
 
+	/**
+	 * Retrieves the root configuration key for the task, based on its ID.
+	 *
+	 * @return string
+	 */
 	protected function TaskRootConfigKey() {
 		return 'Cron.Task.' . $this->CronTaskID;
 	}
 
+	/**
+	 * Retrieves the configuration key for a specific task parameter.
+	 *
+	 * @return string
+	 */
 	protected function TaskConfigKey($SubKey) {
 		return $this->TaskRootConfigKey() . '.' . $SubKey;
 	}
 
+	/**
+	 * Class constructor.
+	 *
+	 * @param string CronTaskID The task ID.
+	 */
 	public function __construct($CronTaskID) {
 		$this->CronTaskID = $CronTaskID;
 	}
 
+	/**
+	 * Returns the time interval to respect between one task execution and the
+	 * next one.
+	 *
+	 * @param int Interval If specified, it replaces the existing interval.
+	 * @return int
+	 */
 	public function RunInterval($Interval = null) {
 		$ConfigKey = $this->TaskConfigKey('RunInterval');
 		$Result = C($ConfigKey, $Interval);
@@ -31,11 +56,23 @@ class TaskTracker {
 
 	}
 
+	/**
+	 * Returns the Unix time stamp of the last task execution.
+	 *
+	 * @return int
+	 */
 	public function LastRun() {
 		$ConfigKey = $this->TaskConfigKey('LastRun');
+		return C($ConfigKey, self::EPOCH);
 	}
 
+	/**
+	 * Determines if the task should run. This is a placeholder method and it must
+	 * be implemented by descendant classes.
+	 *
+	 * @return bool
+	 */
 	public function ShouldRun() {
-		throw new Exception(T('Not implemented. Descendant classes must implement this method.');
+		throw new Exception(T('Not implemented. Descendant classes must implement this method.'));
 	}
 }
